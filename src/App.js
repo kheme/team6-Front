be335db2faf55b0d58025f34bdec06de85354d78
Everistus Olumese <everistusolumese@gmail.com>
2019-05-10 10:47:20 +0100

    Scaffold setup

--- src/App.js
+++ src/App.js
@@ -2,26 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AuthGuard from "./HOCs/AuthGuard";
 import Dashboard from "./Pages/Dashboard";
+import Home from "./Pages/Home";
 
-function Index() {
-  return <h2>Home</h2>;
-}
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Index} />
-      <Route path="/about/" component={About} />
-      <Route path="/users/" component={AuthGuard(Users)} />
-      <Route path="/signin/" component={Dashboard} />
+      <Route path="/" exact component={Home} />
+      
+      <Route path="/users/" component={AuthGuard(Dashboard)} />
+    
     </Router>
   );
 }
